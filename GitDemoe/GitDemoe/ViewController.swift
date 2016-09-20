8ca1287501855be0d50778ea0ede4a6510e5865e
sgww <sgw@siwangyatena.local>
2016-09-20 11:34:50 +0800

    客户端操作
    客户端操作

---
+++ GitDemoe/GitDemoe/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  GitDemoe
+//
+//  Created by sgw on 16/9/20.
+//  Copyright © 2016年 宋关伟. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(1)
+    }
+    
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
